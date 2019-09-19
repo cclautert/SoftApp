211bb84ba101de7146655c1462439ae772089a1d
cristiano lautert <cristiano.c.lautert@gmail.com>
2019-09-18 22:49:35 -0300

    [CHANGE] Ajuste no readme. [CHANGE] Ajustes na classe do Token.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SoftApp
 Software de Cálculo Utilizando 2 API´s
+
+
+Acesso
+
+Api 1 : https://softapptaxa.azurewebsites.net
+Api 2 : https://softappcalculo.azurewebsites.net
+
+A Api 2 utiliza versionamento e possui o endpoint calculajuros na versão v1 e v2. 
+A versão v2 do endpoint calculajuros utiliza Web Token (JWT). 
+Para a obtenção do token utilizar:
+    username: cristiano
+    password: lautert
+
+O token está configurado para expirar a cada 30 minutos.
+
+Infraestrutura A aplicação está dividida em 2 API´s
+
+Estas API's foram desenvolvidas em asp.net Core 2.2, utilizando C#
--- src/SoftApp.Domain/Services/TokenService.cs
+++ src/SoftApp.Domain/Services/TokenService.cs
@@ -17,67 +17,41 @@ namespace SoftApp.Domain.Services
             _configApp = _pConfig;
         }
 
-        public async Task<UserService> Authenticate(LoginService pLogin)
+        public Task<UserService> Authenticate(LoginService pLogin)
         {
-            //return Task.Run(() =>
-            //{
-            //    UserService user = null;
-            //    if (pLogin.Username == "cristiano" && pLogin.Password == "lautert")
-            //    {
-            //        user = new UserService { Name = "Cristiano Lautert", Email = "cristiano.c.lautert@gmail.com" };
-            //    }
-            //    return user;
-            //});            
-
-            UserService user = null;
-
-            if (pLogin.Username == "cristiano" && pLogin.Password == "lautert")
+            return Task.Run(() =>
             {
-                user = new UserService { Name = "Cristiano Lautert", Email = "cristiano.c.lautert@gmail.com" };
-            }
-            return user;
+                UserService user = null;
+                if (pLogin.Username == "cristiano" && pLogin.Password == "lautert")
+                {
+                    user = new UserService { Name = "Cristiano Lautert", Email = "cristiano.c.lautert@gmail.com" };
+                }
+                return user;
+            });
         }
 
-        public async Task<string> BuildToken(UserService user)
+        public Task<string> BuildToken(UserService user)
         {
-            //return Task.Run(() =>
-            //{
-            //    var claims = new[] {
-            //    new Claim(JwtRegisteredClaimNames.Sub, user.Name),
-            //    new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            //    new Claim(JwtRegisteredClaimNames.Birthdate, user.Birthdate.ToString("yyyy-MM-dd")),
-            //    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            //};
-
-            //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configApp.TokenKey));
-            //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            //    var token = new JwtSecurityToken(_configApp.TokenIssuer,
-            //      _configApp.TokenIssuer,
-            //      claims,
-            //      expires: DateTime.Now.AddMinutes(30),
-            //      signingCredentials: creds);
-
-            //    return new JwtSecurityTokenHandler().WriteToken(token);
-            //});            
-
-            var claims = new[] {
+            return Task.Run(() =>
+            {
+                var claims = new[] {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Name),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.Birthdate, user.Birthdate.ToString("yyyy-MM-dd")),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configApp.TokenKey));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configApp.TokenKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(_configApp.TokenIssuer,
-              _configApp.TokenIssuer,
-              claims,
-              expires: DateTime.Now.AddMinutes(30),
-              signingCredentials: creds);
+                var token = new JwtSecurityToken(_configApp.TokenIssuer,
+                  _configApp.TokenIssuer,
+                  claims,
+                  expires: DateTime.Now.AddMinutes(30),
+                  signingCredentials: creds);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            });
         }
     }
 }
